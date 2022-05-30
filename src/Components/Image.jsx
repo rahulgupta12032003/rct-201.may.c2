25255785f474294673a2379b4d8c56acd3a26774
Rahul Gupta <98771367+rahulgupta12032003@users.noreply.github.com>
2022-05-30 17:17:09 +0530

    c2

--- src/Components/Image.jsx
+++
@@ -1,7 +0,0 @@
-export const Image = ({ src, alt, borderRadius, width, height, fit }) => {
-  return (
-    <div>
-      <img />
-    </div>
-  );
-};
--- src/__tests__/App.test.js
+++
@@ -1,5 +0,0 @@
-import { render, screen } from "@testing-library/react";
-
-test("Fake assertion", () => {
-  expect(1).toBe(1);
-});
